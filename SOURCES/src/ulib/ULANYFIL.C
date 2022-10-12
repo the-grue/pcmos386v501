aae6be6a9e0b93decdfd5e2fc1705781c360bfb9
James T. Sprinkle <the-grue@hotmail.com>
2022-10-12 08:58:09 -0700

    Conditional compilation for ULANYFIL.C. Retains backward compatibility with Borland C++.

--- SOURCES/src/ulib/ULANYFIL.C
+++ SOURCES/src/ulib/ULANYFIL.C
@@ -8,7 +8,7 @@
 
  module name:        ulanyfil.c
  creation date:      04/01/92
- revision date:      
+ revision date:
  author:             mjs
  description:        ulib module
 
@@ -21,16 +21,18 @@ mjs 04/01/92	created this module
 
 #include <stdlib.h>
 #include <dos.h>
+#ifdef __BORLANDC__
 #include <dir.h>
+#endif
 #include <io.h>
 
-#include <asmtypes.h>
+#include "asmtypes.h"
 #include "ulib.h"
 
 /*======================================================================
 ;,fs
 ; byte ul_any_files(byte *filespec, byte search_attr)
-; 
+;
 ; in:	filespec -> file specification string (e.g. "c:\\xyz\\abc*.*")
 ;	search_attr = attribute, using FA_NORMAL, FA_RDONLY, etc.
 ;
@@ -43,10 +45,14 @@ mjs 04/01/92	created this module
 byte ul_any_files(byte *filespec, byte search_attr) {
 
   word err_stat;			// holds error status
+
+#ifdef __BORLANDC__
   struct ffblk ffblk;			// for findfirst/next
-
-
   err_stat = findfirst(filespec,&ffblk,search_attr);
+#else
+  struct find_t ffblk;
+  err_stat = _dos_findfirst(filespec,search_attr,&ffblk);
+#endif
   if(err_stat != 0) {
     if(_doserrno == 0x12) {
       return(1);
@@ -58,4 +64,3 @@ byte ul_any_files(byte *filespec, byte search_attr) {
   return(0);
   }
 
-
